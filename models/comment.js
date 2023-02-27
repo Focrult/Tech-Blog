74032b783e4a357d8390afa130769b9ce7b0cbad
Focrult <focrult@gmail.com>
2023-02-27 09:26:05 +0800

    Completed comment.js model

--- models/comment.js
+++ models/comment.js
@@ -2,15 +2,16 @@ const { Model, DataTypes } = require('sequelize');
 
 class Comment extends Model {}
     Comment.init({
-
-
-
-
-
-
-
-
-
+    text:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    user: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+    },
+{
         sequelize,
         timestamps: false,
         freezeTableName: true,
