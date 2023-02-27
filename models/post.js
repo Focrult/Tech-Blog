49ece255f248558884816ca1cc8d5fe4b605f407
Focrult <focrult@gmail.com>
2023-02-27 09:21:25 +0800

    post.js in models finished

--- models/post.js
+++ models/post.js
@@ -3,20 +3,29 @@ const { Model, DataTypes } = require('sequelize');
 class Post extends Model {}
 
 Post.init({
-
-
-
-
-
-
-
-
-
+title:{
+    type: DataTypes.STRING,
+    allowNull: false
+},
+content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'id'
+    }
+  }
+},
+{
     sequelize,
-    timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'post',
 });
-
+Post.belongsTo(User, {
+    foreignKey: 'userID'
+});
 module.exports = Post;
